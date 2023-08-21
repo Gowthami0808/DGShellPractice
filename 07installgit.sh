a3fa3bfee4401d2e6fd5c71a90de5f95a77dde4b
gDoppalapudi <rgowthamiuk@gmail.com>
2023-08-21 20:12:43 +0100

    07gitinstall

--- 07installgit.sh
+++ 07installgit.sh
@@ -7,3 +7,8 @@ else
 # if you give exit 1 before you do echo then it wont even print error it will exit even before that.
    echo "ERROR: You should be a root user"
 fi
+
+if [ $? -ne 0 ]
+then 
+   echo "ERROR: Installation of mysql failed"
+fi
