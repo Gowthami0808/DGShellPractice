f164b1f868d21632b63aecc4612a49551831338b
gDoppalapudi <rgowthamiuk@gmail.com>
2023-08-21 20:00:01 +0100

    07gitinstall

--- 07installgit.sh
+++ 07installgit.sh
@@ -2,7 +2,7 @@
 access=$(id -u)
 if [ $access -eq 0 ]
 then  
-   yum install git -y
+   yum install mysql -y
 else
 # if you give exit 1 before you do echo then it wont even print error it will exit even before that.
    echo "ERROR: You should be a root user"
