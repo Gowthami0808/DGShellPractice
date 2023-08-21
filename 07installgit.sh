0302d727b6e95d95c949b76ca5507836f57d07b7
gDoppalapudi <rgowthamiuk@gmail.com>
2023-08-21 19:58:11 +0100

    07gitinstall

--- 07installgit.sh
+++ 07installgit.sh
@@ -4,5 +4,6 @@ if [ $access -eq 0 ]
 then  
    yum install git -y
 else
+# if you give exit 1 before you do echo then it wont even print error it will exit even before that.
    echo "ERROR: You should be a root user"
 fi
