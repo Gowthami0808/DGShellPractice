3a0656c442b10765244ae7be95d05cda8ffac4e5
gDoppalapudi <rgowthamiuk@gmail.com>
2023-08-22 13:30:19 +0100

    commiting files

--- 10ForLoop.sh
+++ 10ForLoop.sh
@@ -3,11 +3,14 @@ for i in 1 2 3 4 5
 do
    echo "Hi $i"
 done
-for i in {10..15}
+for i in {10..15} #more than two dots won't work
 do
    echo "Welcome $i times"
 done
-for i in {15..25}
+
+#has inbuilt support for setting up a step value using {START..END..INCREMENT} syntax:
+
+for i in {0..10..2}
 do
-   echo "$i"
+  echo "Welcome stepd $i times"
 done
