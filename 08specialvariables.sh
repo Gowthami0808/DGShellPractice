07eb0cef8fd7bdb92f17a5e0e5c062217a57de31
gDoppalapudi <rgowthamiuk@gmail.com>
2023-08-22 11:04:12 +0100

    08specialvariables

--- 08specialvariables.sh
+++ 08specialvariables.sh
@@ -3,7 +3,9 @@
 var1=$1
 var2=$@
 var3=$#
+var4=$0
 echo "the value os var1 is $var1"
 echo "the value os var1 is $var2" #prints all variables
 echo "the value os var1 is $var3" #prints count of all variables
+echo "the value os var1 is $var4" #prints file name
 
