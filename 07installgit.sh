7e4aa20adbb20363e96bf38f2a2feaec6262caae
gDoppalapudi <rgowthamiuk@gmail.com>
2023-08-22 08:59:05 +0100

    commiting logs

--- 07installgit.sh
+++ 07installgit.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 access=$(id -u)
+Date=$(date +"%Y-%m-%d %H:%M:%S")
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$Date.log
+
 if [ $access -eq 0 ]
 then  
-   yum install mysql -y
+   yum install mysql -y &>>$LOGFILE
 else
 # if you give exit 1 before you do echo then it wont even print error it will exit even before that.
    echo "ERROR: You should be a root user"
@@ -12,7 +16,7 @@ if [ $? -ne 0 ]
 then 
    echo "ERROR: Installation of mysql failed"
 fi
- yum install postfix -y
+ yum install postfix -y &>>$LOGFILE
 if [ $? -ne 0 ]
 then 
   echo "Installation of postfix is an error"
@@ -23,9 +27,9 @@ validate()
 {
    if [ $1 -ne 0 ]
    then
-   echo "ERROR with installations of $2"
+   echo "ERROR with installations of $2" &>>$LOGFILE
    else
-   echo "Installations for $2 are success"
+   echo "Installations for $2 are success" &>>$LOGFILE
    fi
 }
 validate $? "Mysql"
