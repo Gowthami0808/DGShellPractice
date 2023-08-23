d3fb485d8c62abd92456be378f2017005236c03d
gDoppalapudi <rgowthamiuk@gmail.com>
2023-08-23 12:56:23 +0100

    commiting delete log files!

---
+++ 11DeleteOldLogs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+APP_LOGS_DIR="/home/centos/app-logs"
+DELETED_FILES_STORAGE="/home/centos/shellscript-logs"
+DATE=$(date +%F)
+SCRIPT_FILE=$0
+LOG_DELETEED_FILES=$DELETED_FILES_STORAGE/$SCRIPT_FILE$DATE.log
+
+
+OLD_LOG_FILES=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
+
+while read line
+do
+   echo "Deleting file $line" &>>$LOG_DELETEED_FILES
+done <<<$OLD_LOG_FILES
